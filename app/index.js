2d80a9a6b225bd8d4026c784d4eeeef631909399
Nate S <n.schelin@hotmail.com>
2016-08-01 11:57:54 -0700

    basic folder creation

--- app/index.js
+++ app/index.js
@@ -1,5 +1,55 @@
 'use strict';
 var generators = require('yeoman-generator');
+var fs = require('fs');
 
+module.exports = generators.Base.extend({
+	constructor: function(){
+		generators.Base.apply(this, arguments);
+	},
+	initializing: function(){
 
+	},
+	prompting: function() {
+
+	},
+	configuring: function() {
+
+	},
+	default: function() {
+
+	},
+	writing: {
+		folders: function() {
+			var styleLibrary = this.destinationRoot() + "/Style Library"
+			var css = styleLibrary + "/css";
+			var js = styleLibrary + "/js";
+			var img = styleLibrary + "/img";
+			fs.mkdirSync(styleLibrary);
+			fs.mkdirSync(css);
+			fs.mkdirSync(js);
+			fs.mkdirSync(img);
+		},
+		gulpfile: function() {
+
+		},
+		packageJSON: function() {
+
+		},
+		git: function() {
+
+		},
+		bower: function() {
+
+		}
+	},
+	conflicts: function() {
+
+	},
+	install: function() {
+
+	},
+	end: function() {
+
+	}
+});
 
